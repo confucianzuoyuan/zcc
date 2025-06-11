f74f42fc96fe3c064de0c65866c8fdca44b2c49a
confucianzuoyuan <zuoyuantc@126.com>
2025-06-11 15:16:38 +0800

    Split cc1 from compiler driver

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
+	"syscall"
 )
 
+var opt_cc1 bool
+var opt_hash_hash_hash bool
 var opt_o string
 var dump_ir bool = false
 var inputPath string = ""
@@ -18,6 +22,16 @@ func usage(status int) {
 
 func parseArgs(args []string) {
 	for idx, arg := range args {
+		if arg == "-###" {
+			opt_hash_hash_hash = true
+			continue
+		}
+
+		if arg == "-cc1" {
+			opt_cc1 = true
+			continue
+		}
+
 		if arg == "--help" {
 			usage(0)
 		}
@@ -55,6 +69,48 @@ func parseArgs(args []string) {
 	}
 }
 
+func runSubprocess(args []string) {
+	// If -### is given, dump the subprocess's command line.
+	if opt_hash_hash_hash {
+		fmt.Fprintln(os.Stderr, strings.Join(args, " "))
+	}
+
+	// 创建命令
+	cmd := exec.Command(args[0], args[1:]...)
+
+	// 将子进程的标准输入/输出/错误继承自当前进程
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// 运行命令并等待完成
+	err := cmd.Run()
+
+	if err != nil {
+		// 如果是退出错误，可以获取退出状态码
+		if exitError, ok := err.(*exec.ExitError); ok {
+			// 获取子进程退出状态
+			if status, ok := exitError.Sys().(syscall.WaitStatus); ok {
+				if status.ExitStatus() != 0 {
+					os.Exit(1)
+				}
+			} else {
+				// 无法获取退出状态，直接退出
+				os.Exit(1)
+			}
+		} else {
+			// 其他错误，比如命令找不到
+			fmt.Fprintf(os.Stderr, "exec failed: %s: %v\n", args[0], err)
+			os.Exit(1)
+		}
+	}
+}
+
+func run_cc1(args []string) {
+	args = append(args, "-cc1")
+	runSubprocess(args)
+}
+
 // openFile 函数接受一个路径字符串，如果路径是 "-"，则返回标准输出。
 // 否则，尝试打开指定路径的文件进行写入。
 func openFile(path string) (*os.File, error) {
@@ -69,10 +125,7 @@ func openFile(path string) (*os.File, error) {
 	return out, nil
 }
 
-func main() {
-	args := os.Args[1:]
-	parseArgs(args)
-
+func cc1() {
 	// Tokenize and parse.
 	tok := tokenizeFile(inputPath)
 	o := parse(tok)
@@ -85,4 +138,16 @@ func main() {
 		fmt.Fprintf(out, ".file 1 \"%s\"\n", inputPath)
 		codegen(o, out)
 	}
+}
+
+func main() {
+	args := os.Args
+	parseArgs(args[1:])
+
+	if opt_cc1 {
+		cc1()
+		return
+	}
+
+	run_cc1(args)
 }
